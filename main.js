ec7a22ba66f6c7c8df6f43ca2d0056f3ec2d0957
theo03iou <theo03iou@outlook.com>
2023-01-14 21:46:05 +0000

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// let num1 = 5;
+// let num2 = 100;
+
+
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+
+
+console.log(add(5000, 300));
+console.log(subtract(5000, 300));
+console.log(multiply(5000, 300));
+console.log(divide(5000, 300));
